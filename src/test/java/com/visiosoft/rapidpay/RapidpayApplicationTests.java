0b0f44e8abd18858daa53066a5a83527bac63f55
Emmanuel Adedayo <emmydee47@gmail.com>
2020-05-10 19:55:48 +0100

    initial commit

---
+++ src/test/java/com/visiosoft/rapidpay/RapidpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visiosoft.rapidpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
